cd744aea452828963e210cc83f6a2d014d728205
matt maier <maier@users.noreply.github.com>
2017-03-29 11:56:02 -0400

    clean up example

--- examples/plugins/native.js
+++ examples/plugins/native.js
@@ -1,12 +1,22 @@
-var mod = function () {
-};
+'use strict';
 
-mod.prototype.run = function(d, cb, req, args, instance) {
-  var data = {
-    now: (+ new Date()),
-    rand: Math.random()
-  };
-  cb(d, data, instance);
-  d.running = false;
-}
-module.exports = mod;
+/* eslint max-params: ["error", 5] */
+/* eslint no-param-reassign: ["error", { "props": false }]*/
+
+module.exports = class MyPlugin {
+    run(plugin_ref, cb, req, plugin_args, plugin_instance) {
+
+        // gather metrics
+        const metrics = {
+            now: Date.now(),
+            rand: Math.random()
+        };
+
+        // indicate the plugin is no longer running
+        plugin_ref.running = false;
+
+        // callback with metrics
+        cb(plugin_ref, metrics, plugin_instance);
+
+    }
+};
